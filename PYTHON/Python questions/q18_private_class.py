ecb773a6df5f5c1b6f8d5ca55450db53d648be7c
Srijan Kumar <shreejansamsung@gmail.com>
2025-08-28 21:59:57 +0530

    update repo

--- PYTHON/Python questions/q18_private_class.py
+++ PYTHON/Python questions/q18_private_class.py
@@ -6,9 +6,15 @@ class Car:
 
     def get_brand(self):
         return self.__brand
+    
+    def get_price(self):
+        return self.__price
 
 c = Car("Toyota", 1500000)
 print(c.get_brand())
+print(c.get_price()) 
+
+# Accessing the private attribute using name mangling
 # print(c.__brand) # Will cause AttributeError
 # The above line is commented out to prevent an error
 # The private attribute __brand cannot be accessed directly outside the class   
